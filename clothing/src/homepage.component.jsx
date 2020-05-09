ff9bd2616d98d27703b1a39e324222f92e132565
Mateusz Kopij <sagasu@gmail.com>
2020-05-09 19:10:58 +0100

    initial commit

---
+++ clothing/src/homepage.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu"></div>
+        <div className="menu-item"></div>
+    </div>
+);
