440c16fd0c3020540af246f7da6d6fb172ab5837
Mateusz Kopij <sagasu@gmail.com>
2020-05-10 10:35:07 +0100

    introducing basic routing

--- clothing/src/App.js
+++ clothing/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react'; 
 import './App.css';
 import { HomePage } from './pages/homepage.component';
+import {Route} from 'react-router-dom'
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS page</h1>
+  </div>
+  
+)
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/hats' component={HatsPage} />
     </div>
   );
 }
