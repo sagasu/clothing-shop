6c09c7a74f6f1c87411b6951618771d206495a82
Mateusz Kopij <sagasu@gmail.com>
2020-05-10 09:56:29 +0100

    improving UI

--- clothing/src/components/menu-item/menu-item.component.jsx
+++ clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './menu-item.styles.scss'
 
-export const MenuItem = ({title, imageUrl, size}) => (
-    <div
-        style = {{
-            backgroundImage: `url(${imageUrl})`
-        }}
-        className={`${size} menu-item`}>
+export const MenuItem = ({ title, imageUrl, size }) => (
+    <div className={`${size} menu-item`}>
+        <div className='background-image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
         <div className="content">
-            <h1 className="title">{title}</h1>
+            <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">shop now</span>
         </div>
+
     </div>
 );
