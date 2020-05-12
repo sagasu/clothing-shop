b170cdbec52686a823bbd1f47a81f13f7c9a1311
Mateusz Kopij <sagasu@gmail.com>
2020-05-12 19:13:44 +0100

    Introducing firebase user management, and sign up component.

--- clothing/src/App.js
+++ clothing/src/App.js
@@ -5,7 +5,7 @@ import  SignInAndSignUp  from './pages/sign-in-sign-up/sign-in-sign-up.component
 import  ShopPage  from './pages/shop/shoppage.component';
 import {Route, Switch} from 'react-router-dom';
 import Header from './components/header/header.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -20,10 +20,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      //
+      //createUserProfileDocument(user);
 
-      console.log(user);
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapshot => {
+          this.setState({currentUser: {
+            id: snapshot.id,
+            ...snapshot.data()
+          }});
+          
+        });
+      }
+
+      this.setState({currentUser: userAuth});
     });
   }
 
