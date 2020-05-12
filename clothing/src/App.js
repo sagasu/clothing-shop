d6dce31dcfebe9f601077f9ce530d274788d1d92
Mateusz Kopij <sagasu@gmail.com>
2020-05-12 21:37:44 +0100

    redirect to home after use signs in, adding types for user.

--- clothing/src/App.js
+++ clothing/src/App.js
@@ -4,7 +4,7 @@ import {connect} from 'react-redux';
 import  HomePage  from './pages/homepage.component';
 import  SignInAndSignUp  from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import  ShopPage  from './pages/shop/shoppage.component';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch,Redirect} from 'react-router-dom';
 import Header from './components/header/header.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import { setCurrentUser} from './redux/user/user.actions';
@@ -45,15 +45,19 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUp} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUp/>)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 }) 
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
