b71e3bb2429401a7f278336aa345565f0dddafeb
Mateusz Kopij <sagasu@gmail.com>
2020-05-22 20:57:26 +0100

    adding functionality to add to cart

--- clothing/src/components/collection-item/collection-item.component.jsx
+++ clothing/src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './collection-item.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
+import {connect} from 'react-redux';
+import {addItem} from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
+const CollectionItem = ({id, name, price, imageUrl, addItem}) => (
 
     <div className="collection-item">
         <div className="image"
@@ -19,4 +21,8 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
     
 )
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem:item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- clothing/src/redux/cart/cart.actions.js
+++ clothing/src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import {CartTypes} from './cart.types';
 export const toggleCardHidden = () => ({
     type: CartTypes.TOGGLE_CARD_HIDDEN
 });
+
+export const addItem = item => ({
+    type: CartTypes.ADD_ITEM,
+    payload: item
+}) 
