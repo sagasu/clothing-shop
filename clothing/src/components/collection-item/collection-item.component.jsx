562212fc97566a63b5ae7d2b7ea53250212839a0
Mateusz Kopij <sagasu@gmail.com>
2020-05-23 17:43:35 +0100

    adding items to store collection.

--- clothing/src/components/collection-item/collection-item.component.jsx
+++ clothing/src/components/collection-item/collection-item.component.jsx
@@ -4,7 +4,9 @@ import CustomButton from '../custom-button/custom-button.component';
 import {connect} from 'react-redux';
 import {addItem} from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({id, name, price, imageUrl, addItem}) => (
+const CollectionItem = ({item, addItem}) => {
+    const {name, price, imageUrl} = item;
+    return (
 
     <div className="collection-item">
         <div className="image"
@@ -16,10 +18,9 @@ const CollectionItem = ({id, name, price, imageUrl, addItem}) => (
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
-        <CustomButton inverted>Add to cart</CustomButton>
+        <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
     </div>
-    
-)
+)};
 
 const mapDispatchToProps = dispatch => ({
     addItem:item => dispatch(addItem(item))
